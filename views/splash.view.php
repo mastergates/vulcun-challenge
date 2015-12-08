8588cc385398601e70931d77d18df2c02e6f4699
Matt Gates <mastergates@users.noreply.github.com>
2015-12-08 16:31:59 -0500

    Create splash.view.php

---
+++ views/splash.view.php
@@ -0,0 +1,16 @@
+<html>
+	<body>
+		<center>
+			<br><br><br><br>
+			<pre>VULCUN CODING CHALLENGE: OPTION B</pre>
+			<pre>CODED BY MATTHEW GATES</pre>
+			<br><br>
+			<pre>Just like the library of babel, all characters are waiting to be discovered</pre>
+			<pre>Everything is created dynamically and saved, so don't worry about losing your stats</pre>
+			<form method="post" action="/stats">
+				<label>View Character Stats</label>&nbsp;
+				<input type="text" id="name" name="name" placeholder="your character is waiting...">
+			</form>
+		</center>
+	</body>
+</html>
